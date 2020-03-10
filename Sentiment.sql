4806e09c937caaf2751ba293fac00d8d138511c6
Jeff Winchell <Jeff_Winchell@g.Harvard.edu>
2020-03-10 18:52:44 -0400

    Sentiment scoring

---
+++ Sentiment.sql
@@ -0,0 +1,10 @@
+Use Test
+Go
+Set NoCount On
+Declare @Return_status int
+Set @Return_Status=-1
+While @Return_Status<>0 Begin
+	Begin Transaction
+	Exec @Return_Status = Tweet_Sentiment
+	Commit
+End
