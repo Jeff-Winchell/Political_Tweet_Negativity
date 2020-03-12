394dadee17c524ba212e112031c265d6a32fb7b3
Jeff Winchell <Jeff_Winchell@g.Harvard.edu>
2020-03-12 10:30:01 -0400

    Change to do no sampling

--- Sentiment.sql
+++ Sentiment.sql
@@ -5,6 +5,6 @@ Declare @Return_status int
 Set @Return_Status=-1
 While @Return_Status<>0 Begin
 	Begin Transaction
-	Exec @Return_Status = Tweet_Sentiment
+	Exec @Return_Status = Tweet_Sentiment 0
 	Commit
 End
